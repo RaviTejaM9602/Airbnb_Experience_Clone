c71a6461e9f738504537e0f155274c953b0118bc
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-02-28 16:51:02 +0000

    Added Navbar component Layout

---
+++ airbnb/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from '../images/airbnb-logo.png';
+
+
+export default function Navbar(){
+  return ( <img src={logo} className="nav--logo" alt="logo" />
+    )
+}
