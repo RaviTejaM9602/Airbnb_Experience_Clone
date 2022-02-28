0eaa445569120e22db164364c28813b7191648c5
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-02-28 21:59:35 +0000

    Add props for rendering Info

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -8,7 +8,13 @@ function App() {
     <div>
            <Navbar />
            <Hero />
-           <Card />
+           <Card 
+           img="katie-zaferes.png"
+           rating="5.0"
+           reviewCount={6}
+           country="USA"
+           title="Life Lessons with Katie Zaferes"
+           price={136}/>
     </div>
   );
 }
--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -3,18 +3,18 @@ import Zaferes from "../images/Zaferes.png";
 import star from "../images/Star.png"
 
 
-export default function Card() {
+export default function Card(props) {
   return (
       <div className="card">
           <img src={Zaferes} className="card--image" alt="Zaferes" />
           <div className="card--stats">
               <img src={star} className="card--star" alt="star-icon" />
-              <span>5.0</span>
-              <span className="gray">(6) • </span>
-              <span className="gray">USA</span>
+              <span>{props.rating}</span>
+              <span className="gray">({props.reviewCount}) • </span>
+              <span className="gray">{props.country}</span>
           </div>
-          <p>Life Lessons with Katie Zaferes</p>
-          <p><span className="bold">From $136</span> / person</p>
+          <p>{props.title}</p>
+          <p><span className="bold">From ${props.price}</span> / person</p>
       </div>
   )
 }
