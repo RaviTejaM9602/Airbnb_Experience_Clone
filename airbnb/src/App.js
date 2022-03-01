1e5875d93f99973e0fa04d05bdfa941f02001460
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-03-01 12:36:44 +0000

    Added Styles for Card Layout

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -21,7 +21,7 @@ function App() {
            <Navbar />
            <Hero />
            <section className="cards-list">
-                {cards}
+                {cardData}
             </section>
     </div>
   );
