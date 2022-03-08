1e4844e17d56c1601d0adafc7af5b96cf03f0805
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-03-08 11:38:15 +0000

    Update logic for calling badge status

--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -4,14 +4,14 @@ import star from "../images/Star.png"
 
 export default function Card(props) {
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
   return (
       <div className="card">
-          {badgeText && <div className="card--badge">SOLD OUT</div>}
+          {badgeText && <div className="card--badge">{badgeText}</div>}
           <img src={`../images/${props.item.coverImg}`} className="card--image" alt="Zaferes" />
           <div className="card--stats">
               <img src={star} className="card--star" alt="star-icon" />
