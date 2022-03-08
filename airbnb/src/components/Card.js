c665d0f4e30bd794dd71478e53ffdd27cdf7026e
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-03-08 11:17:59 +0000

    Added logic for conditional rendering

--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -3,9 +3,15 @@ import star from "../images/Star.png"
 
 
 export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
   return (
       <div className="card">
-          {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+          {badgeText && <div className="card--badge">SOLD OUT</div>}
           <img src={`../images/${props.img}`} className="card--image" alt="Zaferes" />
           <div className="card--stats">
               <img src={star} className="card--star" alt="star-icon" />
